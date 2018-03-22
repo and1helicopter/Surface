f70b22188e00abb02f87edb22dfa0abd85593cae
Denis <and1helicopter@mail.ru>
2018-03-22 16:13:50 +0300

    Initial commit

---
+++ Surface/Chromium.cs
@@ -0,0 +1,50 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows.Forms;
+using System.Xml;
+using CefSharp;
+using CefSharp.WinForms;
+using Surface;
+
+namespace embebbedChromium
+{
+    class CefCustomObject
+    {
+        // Declare a local instance of chromium and the main form in order to execute things from here in the main thread
+        private static ChromiumWebBrowser _instanceBrowser = null;
+        // The form class needs to be changed according to yours
+        private static Form1 _instanceMainForm = null;
+
+
+        public CefCustomObject(ChromiumWebBrowser originalBrowser, Form1 mainForm)
+        {
+            _instanceBrowser = originalBrowser;
+            _instanceMainForm = mainForm;
+        }
+
+        public void showDevTools()
+        {
+            _instanceBrowser.ShowDevTools();
+        }
+        
+        public dynamic saveToFile(dynamic file)
+        {
+            SaveFileDialog sfd = new SaveFileDialog
+            {
+                DefaultExt = @".json",
+            };
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(sfd.FileName, FileMode.Create);
+
+            }
+            return false;
+        }
+
+        //public void opencmd()
+        //{
+        //	ProcessStartInfo start = new ProcessStartInfo("cmd.exe", "/c pause");
+        //	Process.Start(start);
+        //}
+    }
+}
---
+++ Surface/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using embebbedChromium;
+
+namespace Surface
+{
+    public partial class Form1 : Form
+    {
+        private ChromiumWebBrowser _chromeBrowser;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeChromium();
+        }
+
+        private void InitializeChromium()
+        {
+            CefSettings settings = new CefSettings();
+            settings.SetOffScreenRenderingBestPerformanceArgs();
+            DoubleBuffered = true;
+            CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+
+
+
+            // Note that if you get an error or a white screen, you may be doing something wrong !
+            // Try to load a local file that you're sure that exists and give the complete path instead to test
+            // for example, replace page with a direct path instead :
+            // String page = @"C:\Users\SDkCarlos\Desktop\afolder\index.html";
+
+            string page = string.Format($"{ Application.StartupPath}\\view\\index.html");
+            //String page = @"C:\Users\SDkCarlos\Desktop\artyom-HOMEPAGE\index.html";
+
+            if (!File.Exists(page))
+            {
+                MessageBox.Show(@"Error The html file doesn't exists : " + page);
+            }
+
+            // Initialize cef with the provided settings
+            Cef.Initialize(settings);
+            // Create a browser component
+            _chromeBrowser = new ChromiumWebBrowser(page)
+            {
+                RequestContext = new RequestContext()
+            };
+
+            // Add it to the form and fill it to the form window.
+            Panel.Controls.Add(_chromeBrowser);
+            _chromeBrowser.Dock = DockStyle.Fill;
+
+            // Allow the use of local resources in the browser
+            BrowserSettings browserSettings = new BrowserSettings
+            {
+                FileAccessFromFileUrls = CefState.Enabled,
+                UniversalAccessFromFileUrls = CefState.Enabled
+            };
+            _chromeBrowser.BrowserSettings = browserSettings;
+
+            //			var server_start = new CefCustomObject(_chromeBrowser, this);
+            _chromeBrowser.RegisterAsyncJsObject("serverStart", new CefCustomObject(_chromeBrowser, this), BindingOptions.DefaultBinder);
+
+            _chromeBrowser.IsBrowserInitializedChanged += (sender, args) =>
+            {
+                if (_chromeBrowser.IsBrowserInitialized)
+                {
+                    _chromeBrowser.ShowDevTools();
+                }
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (_chromeBrowser.IsBrowserInitialized)
+                _chromeBrowser.ShowDevTools();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Cef.Shutdown();
+        }
+    }
+}
